6c3a4d73e350c1c2be869df77360324d340eb614
Ante Alilovic <aalilovic@etfos.hr>
2020-10-23 15:28:48 +0200

    predaja prve zadace

---
+++ DZ1/ConsoleApp1/ConsoleApp1/Episode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+
+{
+    class Episode
+    {
+        public int viewers;
+        public double scoreSum { get; set; }
+        public double score { get; set; }
+
+        public Episode()
+        {
+            viewers = 0;
+            scoreSum = 0;
+            score = 0;
+        }
+        public Episode(int aViewers, double aScoreSum, double aScore)
+        {
+            viewers = aViewers;
+            scoreSum = aScoreSum;
+            score = aScore;
+        }
+
+        
+        
+        public void AddView(double RandScore) 
+        {
+            viewers += 1;
+            scoreSum += RandScore;
+            if (RandScore > score)
+            {
+                score = RandScore;
+            }
+        }
+        public double GetMaxScore()
+        {
+            return score;
+        }
+        public int GetViewerCount() 
+        {
+            return viewers;
+        }
+        public double GetAverageScore()
+        {
+            double rez;
+            rez = scoreSum / viewers;
+            return rez;
+        }
+
+
+
+    }
+
+
+}
